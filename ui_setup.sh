a84375e70609545702065bef426113cf09d65318
Sean Trott <seantrott@icsi.berkeley.edu>
2015-11-14 11:14:43 -0500

    Modified ui_setup

--- ui_setup.sh
+++ ui_setup.sh
@@ -1,10 +1,6 @@
 #sh starter.sh &
 export ECG_FED=FED2
 export JYTHONPATH=build/compling.core.jar:src/main/nluas/language
-jython -m analyzer ../../ecg-grammars/research.prefs #&
-#jython -m analyzer ../grammars/compRobots.prefs
+jython -m analyzer ../ecg-grammars/research.prefs #&
 
-
-export PID=$!
-#echo "Analyzer" $PID
 #python3 src/main/nluas/language/user_agent.py AgentUI 
