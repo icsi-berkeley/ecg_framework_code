d520d8e6bd2fc505fd00014e5c0c119abf3617a7
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-07 17:09:41 -0800

    Conditionals

--- src/main/nluas/language/core_specializer.py
+++ src/main/nluas/language/core_specializer.py
@@ -80,9 +80,10 @@ class CoreSpecializer(TemplateSpecializer, UtilitySpecializer):
         #else:
         #parameters = self.fill_parameters(eventProcess)
         #ntuple['parameters'] = [parameters]
-        parameters = ntuple['eventDescriptor']['eventProcess']
-        if mood == "wh_question":
-            ntuple['return_type'], ntuple['eventDescriptor']['eventProcess']['specificWh'] = self.get_return_type(parameters)
+        if ['eventProcess'] in ntuple['eventDescriptor']:
+            parameters = ntuple['eventDescriptor']['eventProcess']
+            if mood == "wh_question":
+                ntuple['return_type'], ntuple['eventDescriptor']['eventProcess']['specificWh'] = self.get_return_type(parameters)
 
         ntuple = self.map_ontologies(ntuple)
 
