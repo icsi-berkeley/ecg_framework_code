9432a6175cb632b3b0e74b59f0726e991d724fb0
Sean Trott <seantrott@icsi.berkeley.edu>
2016-03-07 11:18:42 -0800

    spans

--- analyzer.sh
+++ analyzer.sh
@@ -3,4 +3,5 @@ export ECG_FED=FED2
 export JYTHONPATH=build/compling.core.jar:src/main/nluas/language
 jython -m analyzer ../ecg_grammars/research.prefs #&
 
+
 #python3 src/main/nluas/language/user_agent.py AgentUI 
