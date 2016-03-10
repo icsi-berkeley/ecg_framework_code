e00e99a517ef2b28254d320892a2c0f79883b555
Sean Trott <seantrott@icsi.berkeley.edu>
2016-03-10 10:45:19 -0800

    master

--- src/main/nluas/language/analyzer.py
+++ src/main/nluas/language/analyzer.py
@@ -278,5 +278,5 @@ if __name__ == '__main__':
     else:
         if len(sys.argv) != 2:
             usage()
-        #main(sys.argv)
-        analyzer = main2(sys.argv)
+        main(sys.argv)
+        #analyzer = main2(sys.argv)
--- src/main/nluas/language/core_specializer.py
+++ src/main/nluas/language/core_specializer.py
@@ -66,7 +66,6 @@ class CoreSpecializer(UtilitySpecializer):
     def set_spans(self, spans):
         """ Sets the current constructional spans to input spans. These are used for referent resolution. (TO DO)."""
         self.spans = spans
-        print(spans)
 
     def specialize_fragment(self, fs):
         """ Specializes a sentence fragment, e.g. 'the red one' or a non-discourse-utterance. """
